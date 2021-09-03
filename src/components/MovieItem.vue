a54dec3ef33d4796395a2c96d6f616f237607391
Innovanito <khg05311@naver.com>
2021-09-03 14:09:33 +0900

    Third update

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -39,7 +39,14 @@ export default {
   },
   methods: {
     async init() {
-      await this.$loadImage(this.movie.Poster)
+      const poster = this.movie.Poster
+      if(!poster || poster === 'N/A') {
+        this.imageLoading = false
+      } else {
+        await this.$loadImage(poster)
+        this.imageLoading = false
+      }
+      await this.$loadImage(poster)
       this.imageLoading = false
     }
   }
