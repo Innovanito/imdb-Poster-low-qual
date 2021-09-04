52a3ef8fdf3f51a57c8b7f741ce71d394bdbc84b
Innovanito <khg05311@naver.com>
2021-09-04 12:48:46 +0900

    About page updated

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'Heropy',
+    email: 'theSeco@gmail.com',
+    blog: 'htt[://heropy.blog',
+    phone: '0201000301',
+    image: 'http://heropy.blog/css/images/logo.png'
+  })
+}
