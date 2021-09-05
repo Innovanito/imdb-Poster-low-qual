f2a8207a7b8b421d36cff1182b76552434dfae04
Innovanito <khg05311@naver.com>
2021-09-05 17:51:45 +0900

    Vuex helper updated

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,6 +25,7 @@
 
 
 <script>
+import {mapState} from 'vuex'
 import Logo from '~/components/Logo'
 
 export default {
@@ -51,12 +52,10 @@ export default {
     }
   },
   computed: {
-    image() {
-      return this.$store.state.about.image
-    },
-    name() {
-      return this.$store.state.about.name
-    }
+    ...mapState('about', [
+      'image',
+      'name'
+    ])
   },
   methods: {
     isMatch(path) {
